ae1b3600f39f7fb944c6a70690ad4609ee29cdfe
Eduardos13 <edik.kozelsky@gmail.com>
2024-04-11 21:16:58 -0400

    1st task updated

--- js/task-1.js
+++ js/task-1.js
@@ -36,8 +36,6 @@ const users = [
       }
 ];
 
-const getUserNames = users => {
-    return users.map(user => user.name);
-};
+const getUserNames = users => users.map(user => user.name);
 
 console.log(getUserNames(users)); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
